44aa2a14aefc23920f0b5f9439aebd74567f0292
omar0200 <omarminato17@gmail.com>
2026-04-15 19:30:17 +0200

    Question 1

--- G-NET-9-LINQ02/Program.cs
+++ G-NET-9-LINQ02/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var products = new List<Product>
+            {
+                new Product { Name = "Laptop", Price = 1200 },
+                new Product { Name = "Smartphone", Price = 800 },
+                new Product { Name = "Tablet", Price = 600 },
+                new Product { Name = "Headphones", Price = 200 },
+                new Product { Name = "Smartwatch", Price = 300 }
+            };
+            #region Q01
+            var topExpensive = products.Take(3);
+            #endregion
         }
     }
 }
---
+++ G-NET-9-LINQ02/product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_9_LINQ02
+{
+    internal class product
+    {
+    }
+}
