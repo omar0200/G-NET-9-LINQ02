b0e5367edfb2bc4b2f6605b9770b63808430447e
omar0200 <omarminato17@gmail.com>
2026-04-15 19:25:51 +0200

    Add project files.

---
+++ G-NET-9-LINQ02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_9_LINQ02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
