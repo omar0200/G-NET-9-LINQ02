5576515785a3cb9ec9fb28a6398dab72989cd834
omar0200 <omarminato17@gmail.com>
2026-04-15 20:16:08 +0200

    Q13

--- G-NET-9-LINQ02/Program.cs
+++ G-NET-9-LINQ02/Program.cs
@@ -81,11 +81,17 @@
             #endregion
 
             #region Q12
-            var categories = products
-    .Select(p => p.Category)
-    .Distinct();
+            //        var categories = products
+            //.Select(p => p.Category)
+            //.Distinct();
             #endregion
 
+            #region Q13 
+            int[] setA = { 1, 3, 5, 7, 9, 11, 13 };
+            int[] setB = { 3, 6, 9, 12, 15, 13 };
+
+            var result = setA.Except(setB);
+            #endregion
         }
     }
 
